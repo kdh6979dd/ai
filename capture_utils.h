6a54d454f3252a012cbec334e7d10550e6cf145f
aventador <kdh6979dd@gmail.com>
2025-09-09 21:07:12 +0900

    Delete capture_utils.h

--- capture_utils.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <opencv2/opencv.hpp>
-cv::Mat apply_circle_mask(const cv::Mat& input);
-void detect_hsv_contours(const cv::Mat& bgra, std::vector<std::vector<cv::Point>>& out, int min_area=120, cv::Scalar low=cv::Scalar(145,125,125), cv::Scalar high=cv::Scalar(165,255,255));
